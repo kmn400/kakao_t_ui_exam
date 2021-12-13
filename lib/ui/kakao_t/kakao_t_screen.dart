e885458bb8f0d3a9a550dacf2fd8647fb24c8af7
minho <kmn400@nate.com>
2021-12-13 11:55:17 +0900

    12/13 CarouselSlider

--- lib/ui/kakao_t/kakao_t_screen.dart
+++ lib/ui/kakao_t/kakao_t_screen.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:kakao_t_ui_exam/data/fake_data.dart';
 import 'package:kakao_t_ui_exam/ui/kakao_t/component/ad_widget.dart';
@@ -61,24 +62,52 @@ class kakaoTScreen extends StatelessWidget {
   }
 
   Widget _buildAds(PageController controller) {
-    return SizedBox(
-      height: 150,
-      child: PageView(
-        scrollDirection: Axis.horizontal,
-        controller: controller,
-        children: fakeAds.map((e) => AdWidget(ad: e)).toList(),
-        // children: <Widget>[
-        //   AdView(
-        //     ad: fakeAds[0],
-        //   ),
-        //   AdView(
-        //     ad: fakeAds[1],
-        //   ),
-        //   AdView(
-        //     ad: fakeAds[2],
-        //   ),
-        // ],
-      ),
+    return Stack(
+      children: [
+        CarouselSlider(
+          options: CarouselOptions(
+              height: 150,
+              viewportFraction: 0.8,
+              enableInfiniteScroll: true,
+              autoPlay: true,
+              autoPlayCurve: Curves.ease,
+              onPageChanged: (index, _) {
+                setState(() {
+                  _index = index;
+                });
+              }),
+          items: fakeAds.map((Ad e) => AdView(ad: e)).toList(),
+        ),
+        Expanded(
+          child: Align(
+            alignment: Alignment.bottomCenter,
+            child: Row(
+              children: fakeAds.asMap().entries.map((e) {
+                return Container(
+                  width: 12.0,
+                  height: 12.0,
+                  margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 4.0),
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: e.key == _index ? Colors.black : Colors.grey,
+                  ),
+                );
+              }).toList(),
+            ),
+          ),
+        ),
+      ],
+      // children: <Widget>[
+      //   AdView(
+      //     ad: fakeAds[0],
+      //   ),
+      //   AdView(
+      //     ad: fakeAds[1],
+      //   ),
+      //   AdView(
+      //     ad: fakeAds[2],
+      //   ),
+      // ],
     );
   }
 
