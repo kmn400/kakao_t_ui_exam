e8f1372538d54bef4287ece0d3ce96937a2647d9
minho <kmn400@nate.com>
2021-12-15 15:44:18 +0900

    12/13 BMI_CALC

--- lib/ui/bmi_calc/bmi_calc_home_screen.dart
+++ lib/ui/bmi_calc/bmi_calc_home_screen.dart
@@ -1,8 +1,30 @@
 import 'package:flutter/material.dart';
-import 'package:kakao_t_ui_exam/ui/bmi_calc/bmi_calc_result_screen.dart';
 
-class BmiCalcHomeScreen extends StatelessWidget {
-  const BmiCalcHomeScreen({Key key}) : super(key: key);
+import 'bmi_calc_result_screen.dart';
+
+class BmiCalcHomeScreen extends StatefulWidget {
+  @override
+  State<BmiCalcHomeScreen> createState() => _BmiCalcHomeScreenState();
+}
+
+class _BmiCalcHomeScreenState extends State<BmiCalcHomeScreen> {
+  //글로벌 키를 이해하면 물론 좋지만,
+  //이걸 이해 하는데 상당한 노력을 요하기에,
+  //그냥 넘어가도 괜찮다.
+  final _formKey = GlobalKey<FormState>();
+
+  //입력값을 출력하기 위해서 controller를 활용해야 한다.
+  final _heightController = TextEditingController();
+  final _weightController = TextEditingController();
+
+  //화면을 닫기 전에 dispose 함수를 사용해서 메모리에서 해제해야함.
+  //그래야 메모리 과부하가 안 걸림.
+
+  void dispose() {
+    _heightController.dispose();
+    _weightController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,24 +32,68 @@ class BmiCalcHomeScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('BMI 계산기'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            TextField(),
-            TextField(),
-            ElevatedButton(
-              onPressed: () {
-                //버튼 눌렀을 때
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => BmiCalcResultScreen()),
-                );
-              },
-              child: Text('계산'),
+      body: Container(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: <Widget>[
+                TextFormField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: '키',
+                  ),
+                  controller: _heightController, //TextFormField 속성에 컨트롤러가 있음.
+                  keyboardType: TextInputType.number,
+                  validator: (value) {
+                    if (value?.isEmpty ?? false) {
+                      return '키를 입력하세요';
+                    }
+                  },
+                ),
+                SizedBox(
+                  height: 16.0,
+                ),
+                TextFormField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: '몸무게',
+                    ),
+                    controller: _weightController,
+                    keyboardType: TextInputType.number,
+                    validator: (value) {
+                      if (value?.isEmpty ?? false) {
+                        return '몸무게를 입력하세요';
+                      }
+                    }),
+                Container(
+                  margin: const EdgeInsets.only(top: 16.0),
+                  alignment: Alignment.centerRight,
+                  child: ButtonBar(
+                    children: [
+                      ElevatedButton(
+                        onPressed: () {
+                          // 버튼 눌렀을 때
+                          if (_formKey.currentState?.validate() ?? false) {}
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => BmiCalcResultScreen(
+                                    height: double.parse(
+                                        _heightController.text.trim()),
+                                    weight: double.parse(
+                                        _weightController.text.trim()))),
+                          );
+                        },
+                        child: Text('계산'),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
--- lib/ui/bmi_calc/bmi_calc_result_screen.dart
+++ lib/ui/bmi_calc/bmi_calc_result_screen.dart
@@ -1,25 +1,89 @@
 import 'package:flutter/material.dart';
 
 class BmiCalcResultScreen extends StatelessWidget {
-  const BmiCalcResultScreen({Key key}) : super(key: key);
+  //플러터 공부를 할 떄,
+  //책을 통해서 할 수 있는데,
+  //책을 따라서 하다가,
+  //막히는 부분이 있으면,
+  //플러터 공식 문서를 보면서 연습을 하면 된다.
+
+  // HOME에서 컨트롤러를 통해 값을 불러옴.
+  final double height;
+  final double weight;
+
+  BmiCalcResultScreen({
+    Key? key,
+    required this.height,
+    required this.weight,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final double bmi = weight / ((height / 100) * (height / 100));
     return Scaffold(
       appBar: AppBar(
-        title: Text('결과'),
+        title: const Text("비만도 계산기"),
       ),
       body: Center(
-        child: GestureDetector(
-          onTap: () {
-            Navigator.pop(context);
-          },
-          child: Text(
-            '결과 화면',
-            style: TextStyle(fontSize: 20),
-          ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            GestureDetector(
+              onTap: () {
+                Navigator.of(context).pop;
+              },
+              child: Text(
+                _calcBmi(bmi),
+                style: TextStyle(
+                  fontSize: 36,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            _buildIcon(bmi),
+          ],
         ),
       ),
     );
+  }
+
+  String _calcBmi(double bmi) {
+    var result = '저체중';
+    if (bmi >= 35) {
+      result = '고도 미만';
+    } else if (bmi >= 30) {
+      result = '2단계 비만';
+    } else if (bmi >= 25) {
+      result = '1단계 비만';
+    } else if (bmi >= 23) {
+      result = '과체중';
+    } else if (bmi >= 18.5) {
+      result = '정상';
+    }
+    return result;
+  }
+
+  Widget _buildIcon(double bmi) {
+    if (bmi >= 23) {
+      return Icon(
+        Icons.sentiment_very_dissatisfied,
+        color: Colors.red,
+        size: 100,
+      );
+    } else if (bmi >= 18.5) {
+      return Icon(
+        Icons.sentiment_satisfied,
+        color: Colors.green,
+        size: 100,
+      );
+    } else {
+      return Icon(
+        Icons.sentiment_dissatisfied,
+        color: Colors.orange,
+        size: 100,
+      );
+    }
   }
 }
